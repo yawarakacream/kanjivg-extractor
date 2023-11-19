e7591731884661d3f860217ad8c2ba99fcf236b4
yawarakacream <yawarakacream75@gmail.com>
2023-11-19 17:27:55 +0900

    Add radical_clustering

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ from utility import pathstr
 
 input_kvg_path = lambda charcode: pathstr("kanjivg", "kanji", f"{charcode}.svg")
 output_main_kvg_path = lambda charcode: pathstr("output", charcode[:-2] + "00", charcode)
+output_radical_clustering_path = "output-radical-clustering"
+
+font_path = pathstr("~/datadisk/dataset/font/NotoSansJP-Regular.ttf")
--- utility.py
+++ utility.py
@@ -1,4 +1,7 @@
 import os
+from typing import Union
+
+from PIL import Image, ImageDraw, ImageFont
 
 
 def pathstr(*s: str) -> str:
@@ -7,3 +10,35 @@ def pathstr(*s: str) -> str:
 
 def char2code(char):
     return format(ord(char), "#07x")[len("0x"):]
+
+
+def create_vertical_stack_image(data_list: list[Union[str, list[Image.Image]]], gap, font: ImageFont.FreeTypeFont):
+    functions = []
+    width = 0
+    height = 0
+    y = gap
+    for data in data_list:
+        x = gap
+        if isinstance(data, str):
+            dx, dy = font.getsize(data) # type: ignore
+            functions.append((lambda image, draw, x=x, y=y, data=data, font=font: draw.text((x, y), text=data, font=font, fill=0)))
+            x += dx + gap
+            y += dy + gap
+        elif isinstance(data, list):
+            m_dy = 0
+            for data0 in data:
+                dx = data0.width
+                dy = data0.height
+                functions.append((lambda image, draw, x=x, y=y, data0=data0: image.paste(data0, (x, y))))
+                x += dx + gap
+                m_dy = max(m_dy, dy)
+            y += m_dy + gap
+        width = max(width, x)
+    height = y
+
+    image = Image.new("RGB", size=(width, height), color=(255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    for f in functions:
+        f(image, draw)
+
+    return image
