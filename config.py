9d8d40a3348f7a8441fc2a26099bb4416e0cd881
yawarakacream <yawarakacream75@gmail.com>
2023-12-06 17:30:24 +0900

    Add composition

--- config.py
+++ config.py
@@ -2,10 +2,24 @@ from utility import pathstr
 
 
 input_kvg_path = lambda charcode: pathstr("kanjivg", "kanji", f"{charcode}.svg")
-output_main_kvg_path = lambda charcode: pathstr("output", charcode[:-2] + "00", charcode)
+
+output_root_path = pathstr("output")
+
+output_main_kvg_path = lambda charcode: pathstr(
+    output_root_path,
+    "main",
+    charcode[:-2] + "00",
+    charcode
+)
+
 output_radical_clustering_path = lambda dataset_name, n_clusters, image_size, stroke_width, blur: pathstr(
-    "output-radical-clustering",
+    output_root_path,
+    "radical-clustering",
     f"{dataset_name} n_clusters={n_clusters} (imsize={image_size},sw={stroke_width},blur={blur})",
 )
 
-font_path = pathstr("~/datadisk/dataset/font/NotoSansJP-Regular.ttf")
+output_composition_path = lambda composition_name: pathstr(
+    output_root_path,
+    "composition",
+    f"{composition_name}.json"
+)
